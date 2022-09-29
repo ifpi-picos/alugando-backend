65492882dc93caae20d9afde8b9e9341661b0977
Your Name <you@example.com>
2022-09-29 17:31:57 -0300

    qwe

--- src/rotas/index.ts
+++ src/rotas/index.ts
@@ -1,12 +1,14 @@
 import { Router, Request, Response } from 'express';
 import rotasDeUsuarios from './usuarios';
+import rotasDeLogin from './login';
 
-const rotas = Router();
+const rotas = Router(); 
 
 rotas.get('/', (req: Request, res: Response) => {
   res.send('Ola!');
 });
 
 rotas.use('/usuarios', rotasDeUsuarios);
+rotas.use('/login', rotasDeLogin);
 
 export default rotas;
